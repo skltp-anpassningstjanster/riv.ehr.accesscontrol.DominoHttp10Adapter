3e4df665f0d0e0a0cb014996aa95a9be048601dd
hans.thunberg@callistaenterprise.se <hans.thunberg@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2014-08-06 14:30:20 +0000

    MIG-141: DominoHttp10Adapter, Implementera PingFor Configuration i samtliga tjänster ----------------------------------------------------------- Centrala ändringar i denna incheckning -DominoHttp10Adapter/pom.xml, beroende till tjänstekontrakt för itintegration monitoring -PingForconfigurationService.java, PingForConfiguration-rivtabp21-service.xml, PingForconfigurationServiceIntegrationTest -DominoHttp10Adapter-config.properties, nya properties   # PingForConfiguration properties   PINGFORCONFIGURATION_INBOUND_ENDPOINT=http://localhost:11000/tb/itintegration/monitoring/PingForConfiguration/1/rivtabp21   APPLICATION_NAME=DominoHttp10Adapter -DominoHttp10Adapter-soapui-project.xml
    Övriga ändringar kommer från en mvn license:format som behövdes för att få in korrekt licenseinformation i källkodsfilerna

---
+++ DominoHttp10AdapterService/src/test/java/se/skltp/adapterservices/monitoring/PingForconfigurationServiceIntegrationTest.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2014, Inera AB <http://www.inera.se/>. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package se.skltp.adapterservices.monitoring;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.soitoolkit.commons.mule.util.RecursiveResourceBundle;
+
+import se.riv.itintegration.monitoring.v1.PingForConfigurationResponseType;
+
+public class PingForconfigurationServiceIntegrationTest extends FunctionalTestCase {
+
+	private static final RecursiveResourceBundle rb = new RecursiveResourceBundle("DominoHttp10Adapter-config",
+			"DominoHttp10Adapter-config-override");
+
+	private String url = null;
+
+	public PingForconfigurationServiceIntegrationTest() {
+		url = rb.getString("PINGFORCONFIGURATION_INBOUND_ENDPOINT");
+	}
+
+	@Override
+	protected String getConfigResources() {
+		return "PingForConfiguration-rivtabp21-service.xml," + "DominoHttp10Adapter-common.xml";
+	}
+
+	@Test
+	public void pingForConfiguration() throws Exception {
+
+		PingForConfigurationTestConsumer consumer = new PingForConfigurationTestConsumer(url);
+		PingForConfigurationResponseType response = consumer.callService("logicalAddress");
+
+		assertNotNull(response.getPingDateTime());
+		assertEquals("Applikation", response.getConfiguration().get(0).getName());
+		assertEquals(rb.getString("APPLICATION_NAME"), response.getConfiguration().get(0).getValue());
+	}
+
+}
